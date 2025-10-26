8109c754e5b788f358e89511dac4eccabe114c2c
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-10-26 19:12:46 +0530

    main

---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "24320085")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "a9d8e5464a5ac65180bcb48fda55ec66") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8388840881:AAGnitNEpUd3VwJ_XcpCf_A870WhzN7p0Lo") #Replace with your bot token
